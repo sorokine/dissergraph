56b4d14898fb72735062c1b99cdd9e4ebd5e7499
Alex Sorokine <sorokine@gmail.com>
2014-01-19 00:26:16 -0500

    fixes to tooltips

--- README.md
+++ README.md
@@ -4,18 +4,24 @@ dissergraph visuals
 
 ## Dependencies
 
-node.js version 0.10.24
+ * node.js 0.10.24 from http://nodejs.org/
+ * graphviz 2.26.3 from from http://grapphviz.org/ 
 
 ```
 npm install csv
 npm install graphviz
-npm install pass-stream
 
 ```
 
+Tested to work on Ubuntu 12.04 and OS X but should work on any system that supports node.js and graphviz.   
+
 ## Usage
 
-Place ```db.txt``` file in the project directory and run ```node csv2gv.js```.  This will create ```dissergraph.gv``` and ```dissergraph.pdf``` files.  ```dissergraph.gv``` can be viewed using ```xdot``` on Linux of graphviz viewer on OS X (from http://grapphviz.org/).
+Place db.txt file in the project directory and run ```node csv2gv.js```.  This will create dissergraph.gv file that can be viewed using *xdot* on Linux of graphviz viewer on OS X.  To create an SVG file with the diagram run the following command:
+```
+node csv2gv.js && neato -Tsvg dissergraph.gv > dissergraph.svg
+```
+SVG can be viewed in a modern browser like Google Chrome and it has tooltips with the names and titles for each node and edge.
 
 ## Developing
 
--- csv2gv.js
+++ csv2gv.js
@@ -7,16 +7,23 @@ var fs = require('fs');
 //Create digraph G
 var g = graphviz.digraph("G");
 
-// transliterate function from http://stackoverflow.com/questions/11404047/transliterating-cyrillic-to-latin-with-javascript-function
-var a = {"Ё":"YO","Й":"I","Ц":"TS","У":"U","К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH","З":"Z","Х":"H","Ъ":"'","ё":"yo","й":"i","ц":"ts","у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh","щ":"sch","з":"z","х":"h","ъ":"'","Ф":"F","Ы":"I","В":"V","А":"a","П":"P","Р":"R","О":"O","Л":"L","Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v","а":"a","п":"p","р":"r","о":"o","л":"l","д":"d","ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M","И":"I","Т":"T","Ь":"'","Б":"B","Ю":"YU","я":"ya","ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"'","б":"b","ю":"yu"};
+var cyr_lat = {"Ё":"YO","Й":"I","Ц":"TS","У":"U","К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH","З":"Z","Х":"H","Ъ":"'","ё":"yo","й":"i","ц":"ts","у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh","щ":"sch","з":"z","х":"h","ъ":"'","Ф":"F","Ы":"I","В":"V","А":"a","П":"P","Р":"R","О":"O","Л":"L","Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v","а":"a","п":"p","р":"r","о":"o","л":"l","д":"d","ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M","И":"I","Т":"T","Ь":"'","Б":"B","Ю":"YU","я":"ya","ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"'","б":"b","ю":"yu"};
 
+/**
+ * Transliterate cyrillic to latin
+ * from http://stackoverflow.com/questions/11404047/transliterating-cyrillic-to-latin-with-javascript-function
+ * @param word
+ * @returns
+ */
 function translit(word){
   return word.split('').map(function (char) {
-    return a[char] || char;
+    return cyr_lat[char] || char;
   }).join("");
 }
 
-// wordwrap function from http://james.padolsey.com/javascript/wordwrap-for-javascript/
+/**
+ * wordwrap function from http://james.padolsey.com/javascript/wordwrap-for-javascript/
+ */
 function wordwrap( str, width, brk, cut ) {
 	 
     brk = brk || '\n';
@@ -76,42 +83,50 @@ csv()
   	translit(row[3].substring(0,1)) +
 	(row[27].replace(/\s+/g, "") ? (row[27].split(" "))[2] : "NoYear");
   
-  var disser_node = g.addNode( diss_id, {
-	  "label" : wordwrap(row[4].replace(/\"/g, "'").replace(/\n/g, " "), 30, '\\n'), 
-	  "shape" : "box" }
-  );
-  disser_node.set( "style", "filled" );
+  var diss_title = row[4].replace(/\"/g, "'").replace(/\n/g, " ");
+//  var disser_node = g.addNode( diss_id, {
+//	  "label" : wordwrap(row[4].replace(/\"/g, "'").replace(/\n/g, " "), 30, '\\n'), 
+//	  "shape" : "box" }
+//  );
+//  disser_node.set( "style", "filled" );
 
   console.log('#'+index+' '+ '-' + ' '+JSON.stringify(diss_id) + ' ' + row.slice(16,19));
 
   // expects 3-element array of ФИО
   function add_person_node( ФИО ) {
 
+	  ФИО[0] = ФИО[0].replace(/нет( данных)?/i, '')
 	  if (ФИО[0].replace(/\s+/g, "")) 
 		  return g.addNode( translit(ФИО.join('')), {
-			  "label" : ФИО.join('\\n')
+			  "tooltip" : ФИО.join(' '),
+			  "shape" : "circle",
+			  "fixedsize" : "yes",
+			  "height" : "0.2",
+			  "width" : "0.2",
+			  "label" : '',
+			  "style" : "filled"
 		  } );
 	  else
 		  return null;
   }
   
   var author_node = add_person_node(row.slice(1,4));
-  g.addEdge( author_node, disser_node, { "penwidth" : "3", "weight" : 50} );
+  //g.addEdge( author_node, disser_node, { "penwidth" : "3"} );
   
   var adv1_node = add_person_node(row.slice(10,13));
-  if (adv1_node) g.addEdge( adv1_node, disser_node, { "penwidth" : "3", "color" : "green"});
+  if (adv1_node) g.addEdge( adv1_node, author_node, { "penwidth" : "3", "color" : "green", "tooltip" : "руковод. " + diss_title} );
 
   var adv2_node = add_person_node(row.slice(13,16));
-  if (adv2_node) g.addEdge( adv2_node, disser_node, { "penwidth" : "2", "color" : "green"} );
+  if (adv2_node) g.addEdge( adv2_node, author_node, { "penwidth" : "2", "color" : "green", "tooltip" : "руковод. " + diss_title} );
 
   var opp1_node = add_person_node(row.slice(16,19));
-  if (opp1_node) g.addEdge( opp1_node, disser_node, { "color" : "red" } );
+  if (opp1_node) g.addEdge( opp1_node, author_node, { "color" : "red", "tooltip" : "оппон. " + diss_title } );
 
   var opp2_node = add_person_node(row.slice(19,22));
-  if (opp2_node) g.addEdge( opp2_node, disser_node, { "color" : "red" } );
+  if (opp2_node) g.addEdge( opp2_node, author_node, { "color" : "red", "tooltip" : "оппон. " + diss_title} );
 
   var opp3_node = add_person_node(row.slice(22,25));
-  if (opp3_node) g.addEdge( opp3_node, disser_node, { "color" : "red" } );
+  if (opp3_node) g.addEdge( opp3_node, author_node, { "color" : "red", "tooltip" : "оппон. " + diss_title} );
 })
 .on('error', function(error){
   console.log(error.message);
@@ -119,7 +134,7 @@ csv()
 	// Print the dot script
 	fs.writeFileSync( 'dissergraph.gv', g.to_dot() );
 	// Generate a other outputs
-	//g.output( "pdf", "dissergraph.pdf" );
+	//g.output( "svg", "dissergraph.svg" );
 	console.log('done');
 });
 
